6b8f630c55212af1c592bc8a3ca2d2fd884c9b79
Ethan8996 <1979902689@qq.com>
2025-09-07 00:41:31 +0800

    fix: init

---
+++ src/views/Tool1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="tool1">
+        <h1>工具1</h1>
+        <p>这里是工具1的详细内容。</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tool1'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Tool2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="tool2">
+        <h1>工具2</h1>
+        <p>这里是工具2的详细内容。</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tool2'
+}
+</script>
+
+<style>
+</style>
